114e60b63083434a00acf395d86c0c017425b74b
dreamk <dreamk@qq.com>
2024-05-16 17:50:52 +0800

    ✨ feat: 添加交互效果

--- src/pages/publish-homework/index.vue
+++ src/pages/publish-homework/index.vue
@@ -3,18 +3,28 @@
 function handleClickBack() {
   uni.navigateBack()
 }
+
 // 发布班级
 const showHomeworkClass = ref(false)
 const homeworkClass = [
   { id: 'class-1', name: '五年级1班数学' },
   { id: 'class-2', name: '五年级3班数学' },
-  { id: 'class-3', name: '五年级8班数学' },
-  { id: 'class-4', name: '五年级9班数学' },
 ]
-function handleHomeworkClass() {
+const selectedHomeworkClass = ref<Array<string>>([])
+// 关闭选择班级抽屉
+function handleCloseHomeworkClass() {
   showHomeworkClass.value = false
 }
 
+// 选择班级事件
+function handleSelectHomeworkClass(e: { detail: { value: Array<string> } }) {
+  selectedHomeworkClass.value = e.detail.value
+}
+const selectedHomeworkClassContent = computed(() => {
+  return homeworkClass.filter(item => selectedHomeworkClass.value.includes(item.id)).map((item) => {
+    return item.name
+  })
+})
 // 作业类型
 const homeworkType = ['基础', '分组', '分层']
 const curHomeworkType = ref(0)
@@ -22,7 +32,7 @@ function changeHomeworkType(index: number) {
   curHomeworkType.value = index
 }
 
-// 时间
+// 预计时常
 const time = ref(30)
 function changeTime(num: number) {
   if (time.value + num <= 0) {
@@ -34,6 +44,50 @@ function changeTime(num: number) {
   }
   time.value += num
 }
+
+// 发布时间 && 批改结束时间
+function getDate(type: string) {
+  const date = new Date()
+  let year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+
+  if (type === 'start')
+    year = year - 60
+  else if (type === 'end')
+    year = year + 2
+
+  return `${year}-${month > 9 ? month : `0${month}`}-${day > 9 ? day : `0${day}`}`
+}
+const startTime = ref(getDate(''))
+const endTime = ref(getDate(''))
+function handleSelectStartTime(e: { detail: { value: string } }) {
+  startTime.value = e.detail.value
+}
+function handleSelectEndTime(e: { detail: { value: string } }) {
+  endTime.value = e.detail.value
+}
+
+// 批改类型 && 铺码方式
+const correctionType = ref(-1)
+const codingMethod = ref(-1)
+const correctionTypeMessage = computed(() => {
+  if (correctionType.value === -1)
+    return '请选择'
+  else if (correctionType.value === 0)
+    return '批改框批改'
+  else
+    return '采集卡批改'
+})
+
+const codingMethodMessage = computed(() => {
+  if (codingMethod.value === -1)
+    return '请选择'
+  else if (codingMethod.value === 0)
+    return '整班铺码(印刷教辅专用)'
+  else
+    return '一人一码(打印作业专用)'
+})
 </script>
 
 <template>
@@ -70,8 +124,14 @@ function changeTime(num: number) {
             发布班级
           </view>
           <view class="text-right flex gap-15rpx items-center" @tap="showHomeworkClass = true">
-            <view class="text-30rpx text-#8F9AA8">
+            <view v-for="(item, index) in selectedHomeworkClassContent" :key="index" class="px-18rpx py-12rpx bg-#E2F8F0 rounded-30rpx text-22rpx text-#00A76E">
+              {{ item }}
+            </view>
+            <view v-if="selectedHomeworkClass.length === 0" class="text-30rpx text-#8F9AA8">
               请选择
+            </view>
+            <view v-else class="text-24rpx text-#000333">
+              {{ (selectedHomeworkClass.length > 2 ? `等${selectedHomeworkClass.length}个班级` : `${selectedHomeworkClass.length}个班级`) }}
             </view>
             <view class="i-carbon-chevron-right text-#B9C1D0" />
           </view>
@@ -84,15 +144,15 @@ function changeTime(num: number) {
             发布班级
           </view>
           <view class="text-right flex gap-15rpx items-center">
-            <view class="px-18rpx py-12rpx bg-#E2F8F0 rounded-30rpx text-22rpx text-#00A76E">
-              五年级1班数学
+            <view v-for="(item, index) in selectedHomeworkClassContent" :key="index" class="px-18rpx py-12rpx bg-#E2F8F0 rounded-30rpx text-22rpx text-#00A76E">
+              {{ item }}
             </view>
-            <view class="px-18rpx py-12rpx bg-#E2F8F0 rounded-30rpx text-22rpx text-#00A76E">
-              五年级1班数学
-            </view>
-            <view class="flex items-center">
-              <view class="text-24rpx text-#000333">
-                等5个班级
+            <view class="flex gap-15rpx items-center">
+              <view v-if="selectedHomeworkClass.length === 0" class="text-30rpx text-#8F9AA8">
+                上面的
+              </view>
+              <view v-else class="text-24rpx text-#000333">
+                {{ (selectedHomeworkClass.length > 2 ? `等${selectedHomeworkClass.length}个班级` : `${selectedHomeworkClass.length}个班级`) }}
               </view>
               <view class="i-carbon-chevron-right text-#B9C1D0" />
             </view>
@@ -105,10 +165,10 @@ function changeTime(num: number) {
           <view class="text-30rpx text-#000333">
             发布时间
           </view>
-          <picker>
+          <picker v-model="startTime" mode="date" :start="getDate('start')" :end="getDate('end')" @change="handleSelectStartTime">
             <view class="flex gap-15rpx items-center">
               <view class="text-30rpx text-#8F9AA8">
-                请选择
+                {{ startTime }}
               </view>
               <view class="i-carbon-chevron-right text-#B9C1D0" />
             </view>
@@ -118,10 +178,10 @@ function changeTime(num: number) {
           <view class="text-30rpx text-#000333">
             批改结束时间
           </view>
-          <picker>
+          <picker v-model="endTime" mode="date" :start="getDate('start')" :end="getDate('end')" @change="handleSelectEndTime">
             <view class="flex gap-15rpx items-center">
               <view class="text-30rpx text-#8F9AA8">
-                请选择
+                {{ endTime }}
               </view>
               <view class="i-carbon-chevron-right text-#B9C1D0" />
             </view>
@@ -165,10 +225,10 @@ function changeTime(num: number) {
           <view class="text-30rpx text-#000333">
             批改类型
           </view>
-          <picker>
+          <picker :range="['批改框批改', '采集卡批改']">
             <view class="text-right flex gap-15rpx items-center">
               <view class="text-30rpx text-#8F9AA8">
-                请选择
+                {{ correctionTypeMessage }}
               </view>
               <view class="i-carbon-chevron-right text-#B9C1D0" />
             </view>
@@ -181,10 +241,10 @@ function changeTime(num: number) {
             </view>
             <view class="i-carbon-help ml-25rpx text-#adb8c8 h-30rpx w-30rpx" />
           </view>
-          <picker>
+          <picker :range="['整班铺码(印刷教辅专用)', '一人一码(打印作业专用)']">
             <view class="text-right flex gap-15rpx items-center">
               <view class="text-30rpx text-#8F9AA8">
-                请选择
+                {{ codingMethodMessage }}
               </view>
               <view class="i-carbon-chevron-right text-#B9C1D0" />
             </view>
@@ -258,15 +318,15 @@ function changeTime(num: number) {
       mode="bottom"
       size="40%"
       :radius="true"
-      @close="handleHomeworkClass"
+      @close="handleCloseHomeworkClass"
     >
       <template #header>
         <GuoduDrawerHeader title="请选择班级" @close="showHomeworkClass = false" />
       </template>
       <view class="">
-        <checkbox-group class="px-30rpx flex flex-wrap gap-20rpx">
+        <checkbox-group class="px-30rpx flex flex-wrap gap-20rpx" @change="handleSelectHomeworkClass">
           <label v-for="item in homeworkClass" :key="item.id">
-            <checkbox :value="item.id" />{{ item.name }}
+            <checkbox color="#00a76e" :value="item.id" />{{ item.name }}
           </label>
         </checkbox-group>
       </view>
