57664a7d92d6bd8111dc39eb47ad714a28f4635c
dreamk <dreamk@qq.com>
2024-05-17 16:49:04 +0800

    ✨ feat(给家庭作业组件添加插槽，可在多个可在多个页面中使用):

--- src/pages/publish-homework/index.vue
+++ src/pages/publish-homework/index.vue
@@ -124,201 +124,201 @@ function handlePublishHomework() {
 </script>
 
 <template>
-  <view class="bg-#f5f5f9 min-h-100vh">
+  <div class="bg-#f5f5f9 min-h-100vh">
     <!-- 自定义nav-bar -->
-    <view class="px-30rpx pb-60rpx bg-#00A76E">
-      <custom-nav-bar>
+    <div class="px-30rpx pb-60rpx bg-#00A76E">
+      <shy-custom-nav-bar>
         <template #icon>
-          <view class="i-carbon-chevron-left text-white h-35rpx w-35rpx" @tap="handleClickBack" />
+          <div class="i-carbon-chevron-left text-white h-35rpx w-35rpx" @tap="handleClickBack" />
         </template>
         <template #content>
-          <view class="text-32rpx font-bold text-white">
+          <div class="text-32rpx font-bold text-white">
             发布作业
-          </view>
+          </div>
         </template>
-      </custom-nav-bar>
-    </view>
-    <view class="transform-translate-y--25rpx">
+      </shy-custom-nav-bar>
+    </div>
+    <div class="transform-translate-y--25rpx">
       <!-- 作业名称 & 发布班级 -->
-      <view class="px-30rpx pt-40rpx mb-25rpx bg-white rounded-30rpx">
-        <view class="pb-35rpx mb-35rpx border-b b-#EBEBEB b-solid b-l-none b-r-none b-t-none flex">
-          <view class="flex flex-1 flex-col justify-between">
-            <view class="text-24rpx text-#000333">
+      <div class="px-30rpx pt-40rpx mb-25rpx bg-white rounded-30rpx">
+        <div class="pb-35rpx mb-35rpx border-b b-#EBEBEB b-solid b-l-none b-r-none b-t-none flex">
+          <div class="flex flex-1 flex-col justify-between">
+            <div class="text-24rpx text-#000333">
               作业名称
-            </view>
+            </div>
             <input class="placeholder-[#000333] text-32rpx text-#000333" placeholder="请输入作业名称">
-          </view>
-          <view class="h-107rpx w-126rpx">
+          </div>
+          <div class="h-107rpx w-126rpx">
             <image class="h-full w-full" src="../../static/icon/publish-homework/publish-homework.png" mode="aspectFill" />
-          </view>
-        </view>
-        <view class="pb-35rpx flex justify-between">
-          <view class="text-30rpx text-#000333">
+          </div>
+        </div>
+        <div class="pb-35rpx flex justify-between">
+          <div class="text-30rpx text-#000333">
             发布班级
-          </view>
-          <view class="text-right flex gap-15rpx items-center" @tap="showHomeworkClass = true">
-            <view v-for="(item, index) in selectedHomeworkClassMessage" :key="index" class="px-18rpx py-12rpx bg-#E2F8F0 rounded-30rpx text-22rpx text-#00A76E">
+          </div>
+          <div class="text-right flex gap-15rpx items-center" @tap="showHomeworkClass = true">
+            <div v-for="(item, index) in selectedHomeworkClassMessage" :key="index" class="px-18rpx py-12rpx bg-#E2F8F0 rounded-30rpx text-22rpx text-#00A76E">
               {{ item }}
-            </view>
-            <view v-show="selectedHomeworkClass.length === 0" class="text-30rpx text-#8F9AA8">
+            </div>
+            <div v-show="selectedHomeworkClass.length === 0" class="text-30rpx text-#8F9AA8">
               请选择
-            </view>
-            <view class="i-carbon-chevron-right text-#B9C1D0" />
-          </view>
-        </view>
-      </view>
+            </div>
+            <div class="i-carbon-chevron-right text-#B9C1D0" />
+          </div>
+        </div>
+      </div>
       <!-- 发布时间、批改结束时间、作业类型 -->
-      <view class="px-30rpx py-35rpx mb-25rpx bg-white rounded-30rpx">
-        <view class="pb-35rpx mb-35rpx b-b b-b-#EBEBEB b-b-solid flex justify-between items-center">
-          <view class="text-30rpx text-#000333">
+      <div class="px-30rpx py-35rpx mb-25rpx bg-white rounded-30rpx">
+        <div class="pb-35rpx mb-35rpx b-b b-b-#EBEBEB b-b-solid flex justify-between items-center">
+          <div class="text-30rpx text-#000333">
             发布时间
-          </view>
+          </div>
           <picker :value="startTime" mode="date" :start="getDate('start')" :end="getDate('end')" @change="handleSelectStartTime">
-            <view class="flex gap-15rpx items-center">
-              <view class="text-30rpx text-#8F9AA8">
+            <div class="flex gap-15rpx items-center">
+              <div class="text-30rpx text-#8F9AA8">
                 {{ startTime }}
-              </view>
-              <view class="i-carbon-chevron-right text-#B9C1D0" />
-            </view>
+              </div>
+              <div class="i-carbon-chevron-right text-#B9C1D0" />
+            </div>
           </picker>
-        </view>
-        <view class="pb-35rpx mb-35rpx b-b b-b-#EBEBEB b-b-solid flex justify-between items-center">
-          <view class="text-30rpx text-#000333">
+        </div>
+        <div class="pb-35rpx mb-35rpx b-b b-b-#EBEBEB b-b-solid flex justify-between items-center">
+          <div class="text-30rpx text-#000333">
             批改结束时间
-          </view>
+          </div>
           <picker :value="endTime" mode="date" :start="getDate('start')" :end="getDate('end')" @change="handleSelectEndTime">
-            <view class="flex gap-15rpx items-center">
-              <view class="text-30rpx text-#8F9AA8">
+            <div class="flex gap-15rpx items-center">
+              <div class="text-30rpx text-#8F9AA8">
                 {{ endTime }}
-              </view>
-              <view class="i-carbon-chevron-right text-#B9C1D0" />
-            </view>
+              </div>
+              <div class="i-carbon-chevron-right text-#B9C1D0" />
+            </div>
           </picker>
-        </view>
-        <view class="flex justify-between items-center">
-          <view class="text-30rpx text-#000333">
+        </div>
+        <div class="flex justify-between items-center">
+          <div class="text-30rpx text-#000333">
             作业类型
-          </view>
-          <view class="text-right flex gap-30rpx items-center">
-            <view v-for="(item, index) in homeworkType" :key="item" class="flex items-center" @tap="changeHomeworkType(index)">
-              <view class="rounded-1/2 relative h-36rpx w-36rpx" :class="[curHomeworkType === index ? 'bg-#00A76E' : 'b-2 border-solid border-[#d4dce8]']">
-                <view class="bg-white rounded-1/2 absolute h-16rpx w-16rpx left-50% top-50% transform-translate--50%" />
-              </view>
-              <view class="ml-15rpx text-30rpx text-#000333">
+          </div>
+          <div class="text-right flex gap-30rpx items-center">
+            <div v-for="(item, index) in homeworkType" :key="item" class="flex items-center" @tap="changeHomeworkType(index)">
+              <div class="rounded-1/2 relative h-36rpx w-36rpx" :class="[curHomeworkType === index ? 'bg-#00A76E' : 'b-2 border-solid border-[#d4dce8]']">
+                <div class="bg-white rounded-1/2 absolute h-16rpx w-16rpx left-50% top-50% transform-translate--50%" />
+              </div>
+              <div class="ml-15rpx text-30rpx text-#000333">
                 {{ item }}
-              </view>
-            </view>
-          </view>
-        </view>
-      </view>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <!-- 预计时常、批改类型、辅码方式 -->
-      <view class="px-30rpx py-35rpx mb-25rpx bg-white rounded-30rpx">
-        <view class="pb-35rpx mb-35rpx b-b b-b-#EBEBEB b-b-solid flex justify-between items-center">
-          <view class="text-30rpx text-#000333">
+      <div class="px-30rpx py-35rpx mb-25rpx bg-white rounded-30rpx">
+        <div class="pb-35rpx mb-35rpx b-b b-b-#EBEBEB b-b-solid flex justify-between items-center">
+          <div class="text-30rpx text-#000333">
             预计时常(分钟)
-          </view>
-          <view class="text-right flex gap-48rpx items-center">
-            <view class="b-2 b-#ccd0d9 rounded-10rpx b-solid flex-center h-40rpx w-40rpx" @tap="changeTime(-5)">
-              <view class="i-carbon-subtract text-#000333" />
-            </view>
-            <view class="text-30rpx font-bold text-#000333">
+          </div>
+          <div class="text-right flex gap-48rpx items-center">
+            <div class="b-2 b-#ccd0d9 rounded-10rpx b-solid flex-center h-40rpx w-40rpx" @tap="changeTime(-5)">
+              <div class="i-carbon-subtract text-#000333" />
+            </div>
+            <div class="text-30rpx font-bold text-#000333">
               {{ time }}
-            </view>
-            <view class="b-2 b-#ccd0d9 rounded-10rpx b-solid flex-center h-40rpx w-40rpx" @tap="changeTime(5)">
-              <view class="i-carbon-add text-#000333" />
-            </view>
-          </view>
-        </view>
-        <view class="pb-35rpx mb-35rpx b-b b-b-#EBEBEB b-b-solid flex justify-between items-center">
-          <view class="text-30rpx text-#000333">
+            </div>
+            <div class="b-2 b-#ccd0d9 rounded-10rpx b-solid flex-center h-40rpx w-40rpx" @tap="changeTime(5)">
+              <div class="i-carbon-add text-#000333" />
+            </div>
+          </div>
+        </div>
+        <div class="pb-35rpx mb-35rpx b-b b-b-#EBEBEB b-b-solid flex justify-between items-center">
+          <div class="text-30rpx text-#000333">
             批改类型
-          </view>
+          </div>
           <picker :range="['批改框批改', '采集卡批改']" :value="correctionType" @change="handleChangeCorrectionType">
-            <view class="text-right flex gap-15rpx items-center">
-              <view class="text-30rpx text-#8F9AA8">
+            <div class="text-right flex gap-15rpx items-center">
+              <div class="text-30rpx text-#8F9AA8">
                 {{ correctionTypeMessage }}
-              </view>
-              <view class="i-carbon-chevron-right text-#B9C1D0" />
-            </view>
+              </div>
+              <div class="i-carbon-chevron-right text-#B9C1D0" />
+            </div>
           </picker>
-        </view>
-        <view class="flex justify-between items-center">
-          <view class="text-30rpx text-#000333 flex items-center">
-            <view class="text-30rpx">
+        </div>
+        <div class="flex justify-between items-center">
+          <div class="text-30rpx text-#000333 flex items-center">
+            <div class="text-30rpx">
               辅码方式
-            </view>
-            <view class="i-carbon-help ml-25rpx text-#adb8c8 h-30rpx w-30rpx" />
-          </view>
+            </div>
+            <div class="i-carbon-help ml-25rpx text-#adb8c8 h-30rpx w-30rpx" />
+          </div>
           <picker :range="['整班铺码(印刷教辅专用)', '一人一码(打印作业专用)']" :disabled="correctionType === 1" @change="handleChangeCodingMethod">
-            <view class="text-right flex gap-15rpx items-center">
-              <view class="text-30rpx text-#8F9AA8">
+            <div class="text-right flex gap-15rpx items-center">
+              <div class="text-30rpx text-#8F9AA8">
                 {{ codingMethodMessage }}
-              </view>
-              <view class="i-carbon-chevron-right text-#B9C1D0" />
-            </view>
+              </div>
+              <div class="i-carbon-chevron-right text-#B9C1D0" />
+            </div>
           </picker>
-        </view>
-      </view>
+        </div>
+      </div>
       <!-- 设置分数 py比较特殊 -->
-      <view class="px-30rpx py-22rpx mb-25rpx bg-white rounded-30rpx">
-        <view class="flex justify-between items-center">
-          <view class="text-30rpx text-#000333">
+      <div class="px-30rpx py-22rpx mb-25rpx bg-white rounded-30rpx">
+        <div class="flex justify-between items-center">
+          <div class="text-30rpx text-#000333">
             设置分数
-          </view>
-          <view class="flex-center">
+          </div>
+          <div class="flex-center">
             <switch color="#00A76E" :checked="isScore" @change="handleSetScore" />
-          </view>
-        </view>
-      </view>
+          </div>
+        </div>
+      </div>
       <!-- 题目 -->
-      <view class="px-30rpx py-35rpx mb-25rpx bg-white rounded-30rpx">
-        <view class="flex justify-between items-center">
-          <view class="flex items-center">
-            <view class="i-carbon-license-draft text-38rpx text-#00a76e" />
-            <view class="ml-15rpx text-30rpx font-bold text-#000333">
+      <div class="px-30rpx py-35rpx mb-25rpx bg-white rounded-30rpx">
+        <div class="flex justify-between items-center">
+          <div class="flex items-center">
+            <div class="i-carbon-license-draft text-38rpx text-#00a76e" />
+            <div class="ml-15rpx text-30rpx font-bold text-#000333">
               11.28 初中数学
-            </view>
-          </view>
-          <view class="i-carbon-delete text-40rpx text-#fc5260" />
-        </view>
-        <view class="ml-40rpx mt-40rpx text-24rpx text-#000333 flex justify-between">
-          <view>第1页，共9题</view>
-          <view>第2页，共5题</view>
-          <view>第3页，共10题</view>
-        </view>
-      </view>
+            </div>
+          </div>
+          <div class="i-carbon-delete text-40rpx text-#fc5260" />
+        </div>
+        <div class="ml-40rpx mt-40rpx text-24rpx text-#000333 flex justify-between">
+          <div>第1页，共9题</div>
+          <div>第2页，共5题</div>
+          <div>第3页，共10题</div>
+        </div>
+      </div>
       <!-- 题目 -->
-      <view class="px-30rpx py-35rpx mb-25rpx bg-white rounded-30rpx">
-        <view class="flex justify-between items-center">
-          <view class="flex items-center">
-            <view class="i-carbon-license-draft text-38rpx text-#00a76e" />
-            <view class="ml-15rpx text-30rpx font-bold text-#000333">
+      <div class="px-30rpx py-35rpx mb-25rpx bg-white rounded-30rpx">
+        <div class="flex justify-between items-center">
+          <div class="flex items-center">
+            <div class="i-carbon-license-draft text-38rpx text-#00a76e" />
+            <div class="ml-15rpx text-30rpx font-bold text-#000333">
               11.28 初中英语
-            </view>
-          </view>
-          <view class="i-carbon-delete text-40rpx text-#fc5260" />
-        </view>
-        <view class="ml-40rpx mt-40rpx text-24rpx text-#000333 flex justify-between">
-          <view>第1页，共4题</view>
-          <view>第2页，共5题</view>
-          <view>第3页，共6题</view>
-        </view>
-      </view>
+            </div>
+          </div>
+          <div class="i-carbon-delete text-40rpx text-#fc5260" />
+        </div>
+        <div class="ml-40rpx mt-40rpx text-24rpx text-#000333 flex justify-between">
+          <div>第1页，共4题</div>
+          <div>第2页，共5题</div>
+          <div>第3页，共6题</div>
+        </div>
+      </div>
       <!-- 按钮区域 -->
-      <view class="px-30rpx pb-65rpx mt-100rpx flex justify-between">
-        <view class="bg-#FFFFFF rounded-20rpx flex-center h-100rpx w-320rpx">
-          <view class="text-34rpx text-#000333">
+      <div class="px-30rpx pb-65rpx mt-100rpx flex justify-between">
+        <div class="bg-#FFFFFF rounded-20rpx flex-center h-100rpx w-320rpx">
+          <div class="text-34rpx text-#000333">
             添加作业
-          </view>
-        </view>
-        <view class="bg-#00A76E rounded-20rpx flex-center h-100rpx w-320rpx" @tap="handlePublishHomework()">
-          <view class="i-carbon-send-alt-filled mr-14rpx text-32rpx text-#ffffff" />
-          <view class="text-34rpx text-#ffffff">
+          </div>
+        </div>
+        <div class="bg-#00A76E rounded-20rpx flex-center h-100rpx w-320rpx" @tap="handlePublishHomework()">
+          <div class="i-carbon-send-alt-filled mr-14rpx text-32rpx text-#ffffff" />
+          <div class="text-34rpx text-#ffffff">
             发布作业
-          </view>
-        </view>
-      </view>
-    </view>
+          </div>
+        </div>
+      </div>
+    </div>
     <!-- 抽屉 -->
     <GuoduDrawer
       :visible="showHomeworkClass"
@@ -331,13 +331,13 @@ function handlePublishHomework() {
       <template #header>
         <GuoduDrawerHeader class="bg-#f5f5f9 rounded-20rpx" title="请选择班级" @close="showHomeworkClass = false" />
       </template>
-      <view class="bg-#f5f5f9 h-full">
+      <div class="bg-#f5f5f9 h-full">
         <checkbox-group class="px-30rpx text-center text-#000333 grid grid-cols-2 gap-20rpx" @change="handleSelectHomeworkClass">
           <label v-for="item in homeworkClass" :key="item.id" class="p-20rpx bg-white rounded-15rpx text-32rpx flex items-center">
             <checkbox color="#00a76e" :value="item.id" :checked="item.checked" />{{ item.name }}
           </label>
         </checkbox-group>
-      </view>
+      </div>
     </GuoduDrawer>
-  </view>
+  </div>
 </template>
