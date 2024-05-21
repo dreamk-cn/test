d50c32b137926cc5a13b49bd8a6710e19328fe3f
dreamk <dreamk@qq.com>
2024-05-21 18:41:10 +0800

    🎈 perf: 添加占位符，预留出位置

--- src/components/shy-day-selector/shy-day-selector.vue
+++ src/components/shy-day-selector/shy-day-selector.vue
@@ -23,7 +23,18 @@ function selectDay(index: number) {
 
 <template>
   <scroll-view class="whitespace-nowrap" scroll-x :show-scrollbar="false" :scroll-into-view="props.scrollToDayId" scroll-with-animation>
-    <div class="px-20rpx h-full w-full whitespace-nowrap">
+    <div class="h-full w-full whitespace-nowrap">
+      <!-- 占位符 -->
+      <div class="day-item inline-block opacity-0">
+        <div class="mx-auto rounded-20rpx text-36rpx line-height-66rpx text-center h-66rpx w-1rpx transition-opacity">
+          &nbsp;
+        </div>
+        <div class="mx-auto bg-[#00A76E] rounded-1/2 h-12rpx w-12rpx transition-opacity" />
+        <div class="text-22rpx line-height-34rpx text-center text-[#000333] transition-opacity">
+          &nbsp;
+        </div>
+      </div>
+      <!-- 实际渲染数据 -->
       <div v-for="({ id, day, count }, index) in props.dayList" :id="`day-${index}`" :key="id" class="day-item px-10rpx inline-block" @tap="selectDay(index)">
         <div :class="[selectedDayIndex === index ? 'bg-[#00A76E] text-white font-bold' : 'bg-transparent text-[#000333]'] " class="mx-auto rounded-20rpx text-36rpx line-height-66rpx text-center h-66rpx w-66rpx transition-opacity">
           {{ day }}
@@ -31,6 +42,16 @@ function selectDay(index: number) {
         <div class="mx-auto bg-[#00A76E] rounded-1/2 h-12rpx w-12rpx transition-opacity" :class="[(count && props.selectedDayIndex !== index) ? '' : 'opacity-0']" />
         <div class="text-22rpx line-height-34rpx text-center text-[#000333] transition-opacity" :class="[(count && props.selectedDayIndex === index) ? '' : 'opacity-0']">
           {{ count }}份作业
+        </div>
+      </div>
+      <!-- 占位符 -->
+      <div class="day-item inline-block opacity-0">
+        <div class="mx-auto rounded-20rpx text-36rpx line-height-66rpx text-center h-66rpx w-1rpx transition-opacity">
+          &nbsp;
+        </div>
+        <div class="mx-auto bg-[#00A76E] rounded-1/2 h-12rpx w-12rpx transition-opacity" />
+        <div class="text-22rpx line-height-34rpx text-center text-[#000333] transition-opacity">
+          &nbsp;
         </div>
       </div>
     </div>
