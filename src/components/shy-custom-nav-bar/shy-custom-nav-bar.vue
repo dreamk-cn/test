2eae6a584e0a964cf0130a17748aa4600b11b0b0
dreamk <dreamk@qq.com>
2024-05-22 10:28:05 +0800

    🦄 refactor: 重构自定义导航栏

--- src/components/shy-custom-nav-bar/shy-custom-nav-bar.vue
+++ src/components/shy-custom-nav-bar/shy-custom-nav-bar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 const tops = ref(0)
 const height = ref(0)
-onReady(() => {
+onBeforeMount(() => {
   uni.getSystemInfo({
     success: (e) => {
       tops.value = e.statusBarHeight || 0
@@ -19,16 +19,10 @@ onReady(() => {
 </script>
 
 <template>
-  <view>
-    <view :style="[tops ? `height:${tops}px` : `height: 95rpx`]" />
-    <view class="flex justify-between" :style="[height ? `height:${height}px; line-height: ${height}px` : `height: auto; line-height: normal`]">
-      <view class="flex w-250 items-center">
-        <slot name="icon" />
-      </view>
-      <view class="text-center w-250">
-        <slot name="content" />
-      </view>
-      <view class="w-250" />
-    </view>
-  </view>
+  <div>
+    <div :style="[tops ? `height:${tops}px` : `height: ${height}rpx`]" />
+    <div class="flex justify-between" :style="[height ? `height:${height}px; line-height: ${height}px` : `height: auto; line-height: normal`]">
+      <slot />
+    </div>
+  </div>
 </template>
