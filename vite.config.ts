bc4c424a4a17187b4961f8a49bcedac291f3c404
dreamk <dreamk@qq.com>
2024-05-22 18:33:46 +0800

    refactor: 修改vite的代理的目标地址

--- vite.config.ts
+++ vite.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8081/api',
+        target: 'http://192.168.7.34:8081/api',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, ''),
       },
